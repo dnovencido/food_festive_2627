6220a428a2b2c199d3e16df0a657da4f85aa5ada
Denver Novencido <dnovencido@gmail.com>
2026-09-25 10:55:27 +0800

    Delete src directory

--- src/layouts/_messages.php
+++
@@ -1,6 +0,0 @@
-<div class="alert alert-success">
-    <p>
-        <?= $_SESSION['flash_message'] ?>
-        <?php unset($_SESSION['flash_message']); ?>
-    </p>
-</div>
--- src/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "session.php";
-
-    if (array_key_exists("logout", $_GET)) {
-        session_destroy();
-        header("Location: /employee/login");
-    }
-?>
